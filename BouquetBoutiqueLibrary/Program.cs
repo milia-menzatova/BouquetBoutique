f8497c1e1fff89b1d96c1d0c62d018aeb212f8de
Milia Anifieieva <milia.menzatova@gmail.com>
2020-11-23 23:10:33 -0500

    Created project/solution structure

---
+++ BouquetBoutiqueLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BouquetBoutiqueLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
